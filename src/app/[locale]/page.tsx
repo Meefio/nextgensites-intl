6b6221203325ab9132b0e5b3e6d3a1009781855b
Meefio <mic.rowinski@gmail.com>
2025-02-11 20:17:14 +0100

    add navbar with translations

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,12 @@
 import { useTranslations } from 'next-intl'
 import { Hero } from '@/app/components/hero'
+import { Header } from '@/app/components/header'
 
 export default function HomePage() {
 	const t = useTranslations('HomePage')
 	return (
 		<div className='container'>
+			<Header />
 			<main>
 				<Hero />
 			</main>
---
+++ src/app/components/mobile-navbar.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { Menu, X } from 'lucide-react'
+import { ReactNode, useEffect, useState } from 'react'
+import { useTranslations } from 'next-intl'
+
+export function MobileNavbar({ children }: { children: ReactNode }) {
+	const t = useTranslations('Navigation')
+	const [isOpen, setIsOpen] = useState(false)
+
+	useEffect(() => {
+		const overflow = isOpen ? 'hidden' : 'auto'
+		document.documentElement.style.overflow = overflow
+	}, [isOpen])
+
+	useEffect(() => {
+		const closeHamburgerNavigation = () => setIsOpen(false)
+		window.addEventListener(
+			'orientationchange',
+			closeHamburgerNavigation
+		)
+		window.addEventListener('resize', closeHamburgerNavigation)
+
+		return () => {
+			window.removeEventListener(
+				'orientationchange',
+				closeHamburgerNavigation
+			)
+			window.removeEventListener('resize', closeHamburgerNavigation)
+		}
+	}, [])
+
+	return (
+		<>
+			<button
+				className='md:hidden'
+				onClick={() => setIsOpen(!isOpen)}
+				aria-label={isOpen ? t('closeMenu') : t('openMenu')}
+				aria-expanded={isOpen}
+				aria-controls='mobile-menu'
+			>
+				{isOpen ? <X /> : <Menu />}
+			</button>
+			{isOpen && (
+				<div
+					id='mobile-menu'
+					role='dialog'
+					aria-modal='true'
+					aria-label={t('navigationMenu')}
+					className='fixed inset-0 top-[50px] z-40 size-full overflow-auto bg-black/40 animate-in slide-in-from-bottom-24 md:hidden'
+					onClick={() => setIsOpen(false)}
+				>
+					{children}
+				</div>
+			)}
+		</>
+	)
+}
