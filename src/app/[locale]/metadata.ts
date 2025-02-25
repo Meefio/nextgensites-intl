a222a88d3c60782cbc70e76f2185a764844176b3
Meefio <mic.rowinski@gmail.com>
2025-02-25 22:50:38 +0100

    changed something with apple-icon and deleted icons from public, deleted it from metadata.ts

--- src/app/[locale]/metadata.ts
+++ src/app/[locale]/metadata.ts
@@ -47,25 +47,6 @@ export const metadata = (locale: string): Metadata => ({
     creator: '@nextgensites',
     images: '/images/og-image.png',
   },
-  icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
-    other: [
-      {
-        rel: 'mask-icon',
-        url: '/safari-pinned-tab.svg',
-        color: '#020817',
-      },
-    ],
-  },
   category: 'technology',
   other: {
     'color-scheme': 'dark',
